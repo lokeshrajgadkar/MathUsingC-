0d2038bf2588337e1535d5f74a3735d59df73ff5
LokeshR <lokeshrajgadkar13@gmail.com>
2020-10-23 19:58:13 +0530

    Add files via upload

---
+++ addtion of two numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    float a,b,c; 
+    cout << "Enter 1st no: ";
+    cin >> a;
+    cout << "Enter 2nd no: ";
+    cin >> b;
+
+    // sum of two numbers in stored in variable c
+    c = a + b;
+
+    // Prints sum 
+    cout << a << " + " <<  b << " = " << c;     
+
+    return 0;
+}
