9eb31689d19593b51d2d90b20d3a08f12a2e0523
LokeshR <lokeshrajgadkar13@gmail.com>
2020-10-24 16:58:58 +0530

    program of multiplication

---
+++ multiply.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    float a,b,c; 
+    cout << "Enter 1st no: ";
+    cin >> a;
+    cout << "Enter 2nd no: ";
+    cin >> b;
+
+    // multiplication of two numbers in stored in variable c
+    c = a * b;
+
+    // Prints reslut 
+    cout << a << " * " <<  b << " = " << c;     
+
+    return 0;
+}
